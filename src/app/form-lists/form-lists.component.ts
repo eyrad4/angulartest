fd94a098b646bb09ff5963be4c9245a9305dbe3b
eyrad4 <eyrad4@gmail.com>
2017-11-07 19:26:29 +0200

    Создал компонент form-lists который будет отвечать за вывод сохраненных форм

---
+++ src/app/form-lists/form-lists.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-lists',
+  templateUrl: './form-lists.component.html',
+  styleUrls: ['./form-lists.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class FormListsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

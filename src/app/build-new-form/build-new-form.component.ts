abb0a543882b820e102fab34f9989ed5054fed4a
eyrad4 <eyrad4@gmail.com>
2017-11-07 21:28:00 +0200

    Подключил компонент  Routes и RouterModule и прописал базовый роутинг для типовых страниц: главная, новая форма, редактирование форм

---
+++ src/app/build-new-form/build-new-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-build-new-form',
+  templateUrl: './build-new-form.component.html',
+  styleUrls: ['./build-new-form.component.css'],  
+})
+export class BuildNewFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
